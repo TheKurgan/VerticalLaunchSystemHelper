e344952ff2db978b38be35f3fa938a8d139f388a
Kurgun <dunewf@hotmail.com>
2020-12-21 10:02:01 -0500

    Comment out debug lines

--- ModuleVLSMissile.cs
+++ ModuleVLSMissile.cs
@@ -59,13 +59,13 @@ namespace VLSLauncher
                 clearanceRadius = val.clearanceRadius;
                 clearanceLength = val.clearanceLength;
 
-                Debug.Log("[VLS Missile] SAVING ... maxOffBoresight = " + maxOffBoresight);
-                Debug.Log("[VLS Missile] SAVING ... maxTurnRateDPS = " + maxTurnRateDPS);
-                Debug.Log("[VLS Missile] SAVING ... maxTorque = " + maxTorque);
-                Debug.Log("[VLS Missile] SAVING ... clearanceRadius = " + clearanceRadius);
-                Debug.Log("[VLS Missile] SAVING ... clearanceLength = " + clearanceLength);
+                //Debug.Log("[VLS Missile] SAVING ... maxOffBoresight = " + maxOffBoresight);
+                //Debug.Log("[VLS Missile] SAVING ... maxTurnRateDPS = " + maxTurnRateDPS);
+                //Debug.Log("[VLS Missile] SAVING ... maxTorque = " + maxTorque);
+                //Debug.Log("[VLS Missile] SAVING ... clearanceRadius = " + clearanceRadius);
+                //Debug.Log("[VLS Missile] SAVING ... clearanceLength = " + clearanceLength);
 
-                Debug.Log("[VLS Missile] ... ADJUSTING SETTINGS FOR LAUNCH ...");
+                //Debug.Log("[VLS Missile] ... ADJUSTING SETTINGS FOR LAUNCH ...");
 
                 base.part.mass = mass / 100f;
                 val.maxOffBoresight = 270f;
@@ -74,12 +74,12 @@ namespace VLSLauncher
                 val.clearanceRadius = 0.01f;
                 val.clearanceLength = 0.01f;
 
-                Debug.Log("[VLS Missile] CHANGING ... mass = " + part.mass);
-                Debug.Log("[VLS Missile] CHANGING ... maxOffBoresight = " + val.maxOffBoresight);
-                Debug.Log("[VLS Missile] CHANGING ... maxTurnRateDPS = " + val.maxTurnRateDPS);
-                Debug.Log("[VLS Missile] CHANGING ... maxTorque = " + val.maxTorque);
-                Debug.Log("[VLS Missile] CHANGING ... clearanceRadius = " + val.clearanceRadius);
-                Debug.Log("[VLS Missile] CHANGING ... clearanceLength = " + val.clearanceLength);
+                //Debug.Log("[VLS Missile] CHANGING ... mass = " + part.mass);
+                //Debug.Log("[VLS Missile] CHANGING ... maxOffBoresight = " + val.maxOffBoresight);
+                //Debug.Log("[VLS Missile] CHANGING ... maxTurnRateDPS = " + val.maxTurnRateDPS);
+                //Debug.Log("[VLS Missile] CHANGING ... maxTorque = " + val.maxTorque);
+                //Debug.Log("[VLS Missile] CHANGING ... clearanceRadius = " + val.clearanceRadius);
+                //Debug.Log("[VLS Missile] CHANGING ... clearanceLength = " + val.clearanceLength);
 
             }
         }
@@ -89,7 +89,7 @@ namespace VLSLauncher
             MissileLauncher missile = base.part.FindModuleImplementing<MissileLauncher>();
             yield return (object)new WaitForSeconds(delay);
 
-            Debug.Log("[VLS Missile] ... RESET TO SAVED ...");
+            //Debug.Log("[VLS Missile] ... RESET TO SAVED ...");
 
             base.part.mass = mass;
             missile.maxOffBoresight = maxOffBoresight;
@@ -98,12 +98,12 @@ namespace VLSLauncher
             missile.clearanceRadius = clearanceRadius;
             missile.clearanceLength = clearanceLength;
 
-            Debug.Log("[VLS Missile] RESETING TO SAVED ... mass = " + mass);
-            Debug.Log("[VLS Missile] RESETING TO SAVED ... maxTurnRateDPS = " + maxTurnRateDPS);
-            Debug.Log("[VLS Missile] RESETING TO SAVED ... maxOffBoresight = " + maxOffBoresight);
-            Debug.Log("[VLS Missile] RESETING TO SAVED ... maxTorque = " + maxTorque);
-            Debug.Log("[VLS Missile] RESETING TO SAVED ... clearanceRadius = " + clearanceRadius);
-            Debug.Log("[VLS Missile] RESETING TO SAVED ... clearanceLength = " + clearanceLength);
+            //Debug.Log("[VLS Missile] RESETING TO SAVED ... mass = " + mass);
+            //Debug.Log("[VLS Missile] RESETING TO SAVED ... maxTurnRateDPS = " + maxTurnRateDPS);
+            //Debug.Log("[VLS Missile] RESETING TO SAVED ... maxOffBoresight = " + maxOffBoresight);
+            //Debug.Log("[VLS Missile] RESETING TO SAVED ... maxTorque = " + maxTorque);
+            //Debug.Log("[VLS Missile] RESETING TO SAVED ... clearanceRadius = " + clearanceRadius);
+            //Debug.Log("[VLS Missile] RESETING TO SAVED ... clearanceLength = " + clearanceLength);
 
             yield return (object)new WaitForFixedUpdate();
             Object.Destroy(this);
